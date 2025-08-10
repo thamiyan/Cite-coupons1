4e327025503429984f51711dfb74670890e610b8
P-abishan <abishparara@gmail.com>
2025-08-10 19:10:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Container from './components/Container.jsx'
 
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
+import LoginShopper from './pages/LoginShopper.jsx'
+import LoginBusiness from './pages/LoginBusiness.jsx'
 import Signup from './pages/Signup.jsx'
-import Dashboard from './pages/Dashboard.jsx'   // keep if you still want it
+import Dashboard from './pages/Dashboard.jsx'
 import Shopper from './pages/Shopper.jsx'
 import Business from './pages/Business.jsx'
 
@@ -13,7 +15,12 @@ export default function App() {
     <Container>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* login générique (par défaut va vers /shopper) */}
         <Route path="/login" element={<Login />} />
+        {/* routes dédiées */}
+        <Route path="/login/shopper" element={<LoginShopper />} />
+        <Route path="/login/business" element={<LoginBusiness />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/shopper" element={<Shopper />} />
