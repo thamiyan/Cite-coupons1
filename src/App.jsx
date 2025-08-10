31714b402833abb2ed26491d6ae771e24feee8c6
P-abishan <abishparara@gmail.com>
2025-08-10 18:39:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Container from './components/Container.jsx'
+
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import Dashboard from './pages/Dashboard.jsx'   // keep if you still want it
+import Shopper from './pages/Shopper.jsx'
+import Business from './pages/Business.jsx'
 
 export default function App() {
   return (
@@ -13,6 +16,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/shopper" element={<Shopper />} />
+        <Route path="/business" element={<Business />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Container>
